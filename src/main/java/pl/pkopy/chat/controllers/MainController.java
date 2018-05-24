94d1b9f87b5a6413e1945e017715926eea642ac7
pkopy <pawel.kopycki@gmail.com>
2018-05-24 04:53:02 +0200

    Add ChatService

--- src/main/java/pl/pkopy/chat/controllers/MainController.java
+++ src/main/java/pl/pkopy/chat/controllers/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import pl.pkopy.chat.models.ChatEntity;
 import pl.pkopy.chat.models.forms.ChatForm;
 import pl.pkopy.chat.models.repositories.ChatRepository;
+import pl.pkopy.chat.models.services.ChatService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +17,12 @@ import java.util.List;
 @Controller
 public class MainController {
 
-    private String nameTest;
+
 
     @Autowired
     ChatRepository chatRepository;
+    @Autowired
+    ChatService chatService;
 
     @GetMapping("/")
 
@@ -34,7 +37,7 @@ public class MainController {
         ChatEntity chatEntity = new ChatEntity(chatForm);
 
         model.addAttribute("name", chatEntity.getName());
-        nameTest = chatEntity.getName();
+        chatService.setName(chatEntity.getName());
 
         return "conversation";
     }
@@ -50,7 +53,7 @@ public class MainController {
 
     public String conversationPost(@ModelAttribute ChatForm chatForm, Model model){
         ChatEntity chatEntity = new ChatEntity(chatForm);
-        chatEntity.setName(nameTest);
+        chatEntity.setName(chatService.getName());
         chatRepository.save(chatEntity);
         List<ChatEntity> chatEntities = new ArrayList<>();
         chatEntities.addAll(chatRepository.findAllByOrderByIdDesc());
@@ -62,7 +65,7 @@ public class MainController {
         model.addAttribute("allChats", chatEntities);
 
         model.addAttribute("text", chatEntity.getText());
-        model.addAttribute("name",nameTest);
+        model.addAttribute("name",chatService.getName());
 
         return "conversation";
     }
